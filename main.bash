1c91f93ccb0e39b17ccc9bb76e1712868e3469bf
Vandannh <daniel.hagg@live.se>
2019-01-26 17:57:56 +0100

    Fixed on_ready, made a bash script to restart bot when crashing.

---
+++ main.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FAILS=0
+
+while true
+do
+  sleep 0.5
+  ./main.py
+  EXIT=$?
+  ((FAILS++))
+
+  if [[ $FAILS -gt 10 ]]
+  then
+    echo "[$(date)] failed to many times. aborting ..."
+    exit 1
+  fi
+
+  echo "[$(date)] bot exited with code $EXIT. restarting ..."
+
+done
