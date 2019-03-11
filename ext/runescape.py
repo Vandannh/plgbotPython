679976b717c867ae4b9426e45b7eb1e7e62b5eca
Vandannh <daniel.hagg@live.se>
2019-03-11 23:38:59 +0100

    new command: pvst

--- ext/runescape.py
+++ ext/runescape.py
@@ -10,6 +10,27 @@ class Reddit:
 
 
     @commands.command()
+    async def pvst(self, ctx):
+        print("Running command: pvst")
+
+        g_api = "https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="
+
+        def get_subs(yt_name):
+            data = urllib.request.urlopen(g_api + yt_name + "&key=" + config.googleKey).read()
+            subs = json.loads(data.decode("utf-8"))["items"][0]["statistics"]["subscriberCount"]
+            return subs
+
+        subspew = get_subs("PewDiePie")
+        substs = get_subs("tseries")
+        res = """
+PewDiePie:
+{}
+T-Series:
+{}
+Difference:
+{}""".format(int(subspew), int(substs), abs(int(subspew) - int(substs)))
+
+    @commands.command()
     async def runescape(self, ctx, username: str):
         print("Running command: runescape")
         def get_jsonparsed_data(url):
